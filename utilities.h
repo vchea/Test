8a5047f9fd7d2e6ffb781f95ca1e014b08931a1d
jimmyxuan <jimmyxuan@csu.fullerton.edu>
2016-12-09 02:13:07 -0800

    Updated Utilities.h

--- utilities.h
+++ utilities.h
@@ -8,22 +8,49 @@
 
 using namespace std;
 
-// TO DO
-// Read a list of integer, string pairs representing a station name and its identifier from a text file
+
 // return a vector of strings containing the stations names indexed by their identifiers
 // throw an exception if there was a problem opening or reading the file
 std::vector<string> readStationNames(const std::string& filename) {
-
-	throw invalid_argument("Unable to open file " + filename);
-	return std::vector<string>();
+	vector <string> Station(400); 
+	fstream file(filename); 
+	if (!file) {
+		throw invalid_argument("Unable to open file " + filename);
+	}
+	string name;
+	int number; 
+	while (!file.eof())
+	{	
+		file >> name >> number; 
+		Station[number] = name; 
+		
+		
+	}
+	file.close(); 
+	
+	return Station; 
 }
 
-// TO DO
-// Read a list of (integer, integer) pairs representing station identifiers that are next to each other
+
+
 // return a graph representing these station links
 // throw an exception if there was a problem opening or reading the file
 Graph *buildGraphfromDatafile(const std::string& filename) {
+	int route1, route2; 
+	fstream route(filename);
+	Graph map(400);
 
-	throw invalid_argument("Unable to open file " + filename);
-	return NULL;
+	if(!route)
+	{
+		throw invalid_argument("Unable to open file " + filename);
+	}
+
+	while (!route.eof())
+	{
+		route >> route1 >> route2; 
+		map.insertEdge(route1, route2);
+	}
+
+	route.close(); 
+	return &map;
 }
