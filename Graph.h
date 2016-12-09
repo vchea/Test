91fa03804c608f8230d52ecd120665054ec4f614
jimmyxuan <jimmyxuan@csu.fullerton.edu>
2016-12-09 02:12:27 -0800

    Updated Graph.h

--- Graph.h
+++ Graph.h
@@ -1,39 +1,95 @@
-#pragma once
-#include <vector>
+#pragma		once
+#include <vector> 
 
 class Graph {
 public:
-	// TO DO
-	// initialize an undirected graph that can store at most n vertices
+	
+	// initializes an undirected graph that can store at most n vertices
 	Graph(const int n) {
-
+		for (int i = 0; i < 400; i++)
+			for (int k = 0; k < 400; k++)
+				a[i][k] = 0;
 	}
 
-	// TO DO
-	// insert an edge between vertices u and v
+	
+	// inserts an edge between vertices u and v
 	void insertEdge(int u, int v) {
+		a[u][v] = 1; 
+		a[v][u] = 1; 
 
 	}
 
-	// TO DO
-	// return the set of vertices that are adjacent to v
+
+	// returns the set of vertices that are adjacent to v
 	std::vector<int> adjacentVertices(int v) {
 		std::vector<int> vertices;
-
+		for (int i = 0; i < 400; i++) 
+		{
+			if (a[v][i] == 1)
+			{
+				vertices.push_back(i);
+			}
+		}
 		return vertices;
 	}
 
-	// TO DO
+	
 	// return a list of vertices that appear between v and w, starting with v and ending with w
-	// vertices should not be repeated
 	std::vector<int> getPath(int v, int w) {
+		int position = v;
 		std::vector<int> path;
+		std::vector<int> visited; 
+		std::vector <int> possibleRoute = adjacentVertices(v);
+
+		path.push_back(position);
+		visited.push_back(position);
+
+		while (position != w)
+		{
+			
+			if (wasVisited(possibleRoute[0], visited ) == 0){
+			position = possibleRoute[0];
+			possibleRoute = adjacentVertices(position);
+			path.push_back(position);
+			visited.push_back(position);
+			
+			}
+			else {
+				int k = 0;
+				while (wasVisited(possibleRoute[k], visited) == 1) {
+					if (k < (possibleRoute.size()-1)) {
+						k++;
+					}
+					else {
+						k = 0; 
+						path.pop_back(); 
+						position = path.back(); 
+						possibleRoute = adjacentVertices(position); 
+						
+					}
+				}
+				position = possibleRoute[k];
+				possibleRoute = adjacentVertices(position);
+				path.push_back(position);
+				visited.push_back(position);
+			
+			}
+		}
 
 		return path;
 	}
 
 private:
-	// TO DO
 	// member variables and functions to implement the public member functions
+	int a[400][400];
 
+	bool wasVisited(int station, std::vector <int> Visited)
+	{
+		for (int i = 0; i < Visited.size(); i++)
+		{
+			if (station == Visited[i])
+				return 1;
+		}
+		return 0; 
+	}
 };
